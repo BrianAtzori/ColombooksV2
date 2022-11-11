df0cdb07b4102b524ba5c4b7cff66283695f3e14
BrianAtzori <brianatzori@gmail.com>
2022-11-11 08:30:32 +0100

    Adjust info button output after clicking details

--- src/scripts/pop-ups.js
+++ src/scripts/pop-ups.js
@@ -20,7 +20,15 @@ export function generatePopUp(popUpType,data)
     switch(popUpType){
 
         case 'info':
+            
+            popUpWindowTitle.innerHTML = "Guide to Colombooks"
+
+            //Set to guide if i click the info button after clicking details button
+
+            popUpWindowBody.innerHTML = "<p>Hello! Welcome* to the <strong>Colombooks</strong> guide :)<br><br></p><p>This is a small application that through the <strong>openlibrary.org</strong> API will allow you to search for books within an online archive.<br><br></p><p>On Colombooks you will be able to perform three types of searches:<br><br></p><p>1)<strong>By genre</strong>: typing the name of a genre Colombooks will return the books that according to the archive are found to belong to that genre.&nbsp;<br><br></p><p>2)<strong>By author</strong>: entering the name of your favorite author will search for all the books that that author has written or contributed to.<br><br></p><p>3)<strong>By title</strong>: search for any book by entering the title or part of the title, and Colombooks will return books that match the words you enter!<br><br></p><p>By clicking on the details you will be able to view the <strong>description</strong> of the book!<br><br></p><p><span style='text-decoration: underline;'><strong>!!!!WARNING!!!</strong></span> Some books are missing some data, such as covers and descriptions.<br><br></p><p>This app was developed as a study project by <strong>Brian Atzori</strong>, a student at the online university <strong>Start2Impact</strong>.<br><br></p><p>&nbsp;</p>"
+
             popUpWindow.classList.add('active')
+
             break;
 
         case 'details':
